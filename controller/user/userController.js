0de1674944e6853ba4e3bd6d791195dd5a634c5f
Bhairab-Nath <nathbhairab22@gmail.com>
2025-08-17 22:34:38 +0545

    Login feature

--- controller/user/userController.js
+++ controller/user/userController.js
@@ -6,12 +6,53 @@ exports.renderRegister = (req,res)=>{
 }
 
 exports.register = async(req,res)=>{
-    console.log(req.body)
+    // console.log(req.body)
     const {username, email, password } = req.body
+    const user = users.findAll({
+        where: {
+            email:email
+        }
+    })
+
+    if(user.length != 0){
+        return res.send("Email already Registered! Login")
+    }
+
     await users.create({
         username : username,
         email: email,
         password: bcrypt.hashSync(password,12) //12 is salt
     })
-    res.send("Registered Successfully!")
+    res.redirect('/login')
+}
+
+exports.renderLoginForm =  (req,res)=>{
+    res.render('login')
+}
+
+exports.loginForm = async (req,res)=>{
+    // console.log(req.body)
+    const {email, password} = req.body
+    if(!email || !password){
+       return res.send("Please fill both email and password!")
+    }
+
+   const user = await users.findAll({
+        where: {
+            email: email
+        }
+    })
+
+    if(user.length === 0){
+        res.send("No user with that email!")
+    }else{
+        const isMatched = bcrypt.compareSync(password, user[0].password)
+        if(isMatched){
+            res.send("Login Successfully")
+        }else{
+            res.send("Email or Password is invalid")
+        }
+    }
+
+
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
-const { renderRegister, register } = require('../controller/user/userController')
+const { renderRegister, register, renderLoginForm, loginForm } = require('../controller/user/userController')
 
 const router = require('express').Router()
 
 router.route('/register').get(renderRegister)
 router.route('/register').post(register)
-
+router.route('/login').get(renderLoginForm).post(loginForm)
 
 
 
