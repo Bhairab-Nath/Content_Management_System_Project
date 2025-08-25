979199630ff6d1dffe9e64747c0013c7abc980fa
Bhairab-Nath <nathbhairab22@gmail.com>
2025-08-25 15:12:22 +0545

    Async Error Handling

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,6 +1,7 @@
 const { renderHome, renderAddBlog, addBlog, renderRegister, renderSingleBlog, register, deleteBlog, renderUpdateBlog, updateBlog } = require("../controller/blog/blogController")
 const { isAuthenticated } = require("../middleware/isAuthenticated.js")
 const {storage, multer} =require('../middleware/multerConfig.js')
+const catchError = require("../services/catchError.js")
 const upload = multer({storage: storage})
 
 const router = require("express").Router()
@@ -14,7 +15,7 @@ router.route('/addblogs').post(upload.single("image"), isAuthenticated, addBlog)
 router.route('/blog/:id').get(renderSingleBlog)
 router.route('/delete/:id').get(deleteBlog)
 router.route('/update/:id').get(renderUpdateBlog)
-router.route('/update/:id').post(updateBlog)
+router.route('/update/:id').post(catchError(updateBlog))
 
 
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,16 @@
 const { renderRegister, register, renderLoginForm, loginForm, logoutUser, forgetPassword, forgetPasswordHandle, renderOtpForm, verifyOtp, renderResetPassword, handleResetPassword } = require('../controller/user/userController')
+const catchError = require('../services/catchError')
 
 const router = require('express').Router()
 
 router.route('/register').get(renderRegister)
-router.route('/register').post(register)
-router.route('/login').get(renderLoginForm).post(loginForm)
+router.route('/register').post(catchError(register))
+router.route('/login').get(renderLoginForm).post(catchError(loginForm))
 router.route('/logout').get(logoutUser)
-router.route('/forgetPassword').get(forgetPassword).post(forgetPasswordHandle)
+router.route('/forgetPassword').get(forgetPassword).post(catchError(forgetPasswordHandle))
 router.route('/otpForm').get(renderOtpForm)
-router.route('/verifyOtp/:id').post(verifyOtp)
+router.route('/verifyOtp/:id').post(catchError(verifyOtp))
 router.route('/resetPassword').get(renderResetPassword)
-router.route('/resetPassword/:email/:otp').post(handleResetPassword)
+router.route('/resetPassword/:email/:otp').post(catchError(handleResetPassword))
 
 module.exports = router
